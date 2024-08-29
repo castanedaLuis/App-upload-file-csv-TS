b3566dee10743ed309e882ebe29915c79ab02459
Jose Luis Castañeda <programador1996@outlook.es>
2024-08-29 11:40:37 -0600

    feat: :sparkles: Create endponit POST, to upload csv, and transform data to json, and save on memory data.
    Create endponit POST, to upload csv, and transform data to json, and save on memory data.

--- BackEnd/server.ts
+++ BackEnd/server.ts
@@ -1,18 +1,50 @@
 import express from 'express'
 import cors from 'cors'
+import multer from 'multer'
+import csvToJson from 'convert-csv-to-json'
 
 
 const app = express()
 const port =  3000
 
+const storage = multer.memoryStorage()
+const upload = multer({ storage:storage })
+
+//un Array de objetos
+let userData:Array<Record<string,string>> = []
 
 app.use(cors())
 
-app.post('/api/files', async(req,res) =>{
+app.post('/api/files',upload.single('file') ,async(req,res) =>{
     //Extract file the frontend
+    const { file } = req
+    //Validate exist file
+    if(!file){
+        return res.status(500).json({ message : "File is required"})
+    }
+    //Validate tipe the file
+    if(file.mimetype != 'text/csv') return res.status(500).json({ message : "File must be csv"})
 
+    let json:Array<Record<string,string>> = []
+    try {
+         //Transform file buffer to string
+        const csv = Buffer.from(file.buffer).toString('utf-8')
+        console.log(csv);
+        //Transform string to JSON
+        json  = csvToJson.csvStringToJson(csv)
+    } catch (error) {
+        return res.status(500).json({ message : "Error parsing Array File"})
+    }
 
-    return res.status(200).json({data:[], message:"El archivo se cargo correctamente"})
+    //save json to memory DB
+    userData = json
+    // return messgae with data(JSON)
+    return res.status(200).json({data:json, message:"El archivo se cargo correctamente"})
+})
+
+app.get('/api/user',async (req,res)=>{
+
+    return res.status(200).json({data:[]})
 })
 
 app.listen(port ,()=>{
